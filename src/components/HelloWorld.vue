5e10e4ab2e5dc01625fa4a33d2c173d6f1756cf8
최원빈 <chldnjsqls1264@gmail.com>
2019-09-03 20:43:24 +0900

    fix statistics

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>This is board statistics</h1>
+    <h1>It is fixed</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
