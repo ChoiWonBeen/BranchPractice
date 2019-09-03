a93d9a1595b4c28c0434956c5b788df77e881a8b
최원빈 <chldnjsqls1264@gmail.com>
2019-09-03 22:30:29 +0900

    change text more change

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>It is fixed fixfixed</h1>
     <p>
-      changing
+      changing more more!
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
