2dbbbabfdde71e4e85550612112edef11c2129f3
최원빈 <chldnjsqls1264@gmail.com>
2019-09-03 22:29:46 +0900

    change main text

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,8 +2,7 @@
   <div class="hello">
     <h1>It is fixed fixfixed</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
+      changing
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
